e1cabc73ad19c5ac795fe2740d64c65364509c3d
brachar <bracha.revah@danhotels.com>
2021-02-13 22:31:12 +0200

    add property

--- src/AppDragDrop.js
+++ src/AppDragDrop.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 export default class AppDragDropDemo extends Component {
     state = {
         imgs: [
-            {name: "demo", src: require("./assets/img.jpg"), category: "warp"}
+            {name: "demo", src: require("./assets/img.jpg"), category: "warp", class: "draggable"}
           ]
     }
 
     onDragStart = (ev, id) => {
         console.log('dragstart:',id);
+
+
+        // const el = document.getElementById('drag');
+        // el.classList.add("rotate");
+
+        // ev
+        // .dataTransfer
+        // .setData('text/plain', ev.target.id);
+        
+       
+        // ev
+        // .currentTarget
+        // .style
+        // .transform = 'rotate(30deg)';
+
+        // ev.dataTransfer.setData("class", "rotate");
+
+   
+
         ev.dataTransfer.setData("id", id);
     }
 
     onDragOver = (ev) => {
+        
         ev.preventDefault();
+
+    }
+
+    onDragTest = (ev) => {
+
+        // const el = document.getElementById('drag');
+        // el.classList.add("rotate");
+
+        // ev
+        // .dataTransfer
+        // .setData('text/plain', ev.target.id);
+       
+        // ev
+        // .currentTarget
+        // .style
+        // .transform = 'rotate(30deg)';
+
+        ev
+        .currentTarget
+        .style
+        .display = 'none';
     }
 
     onDrop = (ev, cat) => {
@@ -44,9 +86,11 @@ export default class AppDragDropDemo extends Component {
                 <img key={t.name} 
                     src={t.src}
                     onDragStart = {(e) => this.onDragStart(e, t.name)}
-                    draggable
-                    className="draggable"
+                    draggable={true}
+                    onDrag={(e) => this.onDragTest(e)}
+                    className={t.class}
                     alt={t.name}
+                    id="drag"
                 />
             );
         });
@@ -59,13 +103,13 @@ export default class AppDragDropDemo extends Component {
                     <span className="task-header">Drag the image</span>
                     {imgs.warp}
                 </div>
+                {/* <div id="coverup"></div> */}
                 <div className="droppable" 
                     onDragOver={(e)=>this.onDragOver(e)}
                     onDrop={(e)=>this.onDrop(e, "complete")}>
                      <span className="task-header">Drag the image here</span>
                      {imgs.complete}
                 </div>
-
 
             </div>
         );
